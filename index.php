75e9e149beff5067ddff19d4efffe40613e89c0b
TylerMcKell-DOMO <tyler.mckell@domo.com>
2025-01-27 10:07:04 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/styles/index.css">
+    <title>"Race Entry"</title>
+</head>
+<body>
+    <div class="header">
+        <div class="left">
+            <a href="/index.php">Participants</a>
+            <a href="/index.php">Times</a>
+            <a href="/index.php">Directors</a>
+        </div>
+        <div class="right">
+            <a href="">login</a>
+            <a href="">create race</a>
+        </div>
+    </div>
+    <div class="hero">
+        <div class="top">
+            <div class="title">Online Race Registration</div>
+            <div class="text">It's free and as easy as 1, 2, 3.
+                Create your account, design your
+                form, and grow your race.
+            </div>
+            <div class="button"><button>Create a Race</button></div>
+        </div>
+        <div class="bottom">
+            <div class="left">Krispy Kreme Challenge</div>
+            <div class="right">“Race Entry has the best race registration software, volunteer management, fundraising campaigns and product shop all in one place.”</div>
+        </div>
+    </div>
+</body>
+</html>
